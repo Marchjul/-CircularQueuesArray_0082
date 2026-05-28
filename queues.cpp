ad84ff425a2b323b455c03a605d80cca8aaa4f12
Marchjul <marchellogiovani28@gmail.com>
2026-05-28 21:01:22 +0700

    Implement remove function for queue management
    Added a remove function to handle queue deletion and check for underflow.

--- queues.cpp
+++ queues.cpp
@@ -48,3 +48,11 @@ void insert()
     }
 
 void remove()
+{
+        // ccek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
