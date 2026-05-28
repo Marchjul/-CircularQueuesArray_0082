9ee3714409a1745acde174e585752c6d8f05f39c
Marchjul <marchellogiovani28@gmail.com>
2026-05-28 21:03:17 +0700

    Implement wrap-around for FRONT index in dequeue
    Add logic to wrap FRONT index when removing last element

--- queues.cpp
+++ queues.cpp
@@ -65,3 +65,11 @@ void remove()
         }
         else
         {
+
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
